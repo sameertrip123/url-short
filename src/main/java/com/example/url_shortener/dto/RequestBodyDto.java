10c87581486359d26ec734a327b1a6a29a5b2c63
sameer tripathi <sameertrip123@gmail.com>
2024-08-02 18:42:41 +0530

    Added User Input Validation

--- src/main/java/com/example/url_shortener/dto/RequestBodyDto.java
+++ src/main/java/com/example/url_shortener/dto/RequestBodyDto.java
@@ -1,8 +1,14 @@
 package com.example.url_shortener.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class RequestBodyDto {
-    private String url;
+
+	@NotNull(message = "URL cannot be NULL")
+	@NotBlank(message = "URL cannot be blank")
+	private String url;
+
 }
